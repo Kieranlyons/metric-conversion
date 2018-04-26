c0a3bdd79ba8c229ba8157ef7f38510fbcfdfc89
Kieranlyons <36700611+Kieranlyons@users.noreply.github.com>
2018-04-26 16:26:47 +0100

    Basic functionality of all 4 buttons

--- convert.js
+++ convert.js
@@ -1,3 +1,6 @@
+//4 seperate functions
+
+//corrected code issues using console log
 function fTom() {
 
   var measure = parseInt(document.getElementById("value1").value);
@@ -9,4 +12,42 @@ function fTom() {
   console.log(message);
   document.getElementById("resultsentence").innerHTML = message;
 
+}
+//inches to centimetres conversion 
+//copied and pasted code and changed equations and /"qoutes/ for output assigned appropriately in html
+function iToc() {
+
+  var measure = parseInt(document.getElementById("value1").value);
+
+  var cmeters = measure * 2.54;
+  var message = measure + ' inches converts to ' + cmeters + ' centimetres.';
+
+
+  console.log(message);
+  document.getElementById("resultsentence").innerHTML = message;
+}
+
+//yards to miles conversion
+function yTom() {
+
+  var measure = parseInt(document.getElementById("value1").value);
+
+  var ymeters = measure * 0.9144;
+  var message = measure + ' Yards converts to ' + ymeters + ' metres.';
+
+
+  console.log(message);
+  document.getElementById("resultsentence").innerHTML = message;
+}
+//miles to kilometres in a lapse of memory unable to place buttons beside each other
+function mTok() {
+
+  var measure = parseInt(document.getElementById("value1").value);
+
+  var mkilolmeters = measure * 1.60934;
+  var message = measure + ' Yards converts to ' + mkilolmeters + ' kilometres.';
+
+
+  console.log(message);
+  document.getElementById("resultsentence").innerHTML = message;
 }
