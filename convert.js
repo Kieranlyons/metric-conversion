da1fb57298260720408466cde97dc84578977815
Kieranlyons <36700611+Kieranlyons@users.noreply.github.com>
2018-04-26 17:03:44 +0100

    Basic math working, alerts 1 in place
    could not figure out issue with 0 value and no input value at all
    
    tried swapping alert tags around on foot to meters but no change.
    
    was in the home straight but failed.

--- convert.js
+++ convert.js
@@ -1,9 +1,16 @@
 //4 seperate functions
 
-//corrected code issues using console log
+//corrected code issues using console log// tried swapping alert tags around but still no change in output
 function fTom() {
 
   var measure = parseInt(document.getElementById("value1").value);
+  
+     if(measure<=0.1){
+    alert("Values must be greater than 0");
+  } else if(!measure){
+    alert("Please enter a value");
+ }
+  else{
 
   var meters = measure * 0.3048;
   var message = measure + ' feet converts to ' + meters + ' meters.';
@@ -11,13 +18,19 @@ function fTom() {
 
   console.log(message);
   document.getElementById("resultsentence").innerHTML = message;
-
+  }
 }
 //inches to centimetres conversion 
 //copied and pasted code and changed equations and /"qoutes/ for output assigned appropriately in html
 function iToc() {
 
   var measure = parseInt(document.getElementById("value1").value);
+   if(!measure){
+    alert("Please input a value !");
+  } else if(measure<0.1){
+    alert("Values entered must be greater than zero !");
+ }
+  else{
 
   var cmeters = measure * 2.54;
   var message = measure + ' inches converts to ' + cmeters + ' centimetres.';
@@ -26,12 +39,19 @@ function iToc() {
   console.log(message);
   document.getElementById("resultsentence").innerHTML = message;
 }
+}
 
-//yards to miles conversion
+//yards to metres conversion , cannot get else if statement running correctly 
 function yTom() {
-
-  var measure = parseInt(document.getElementById("value1").value);
-
+	
+	var measure = parseInt(document.getElementById("value1").value);
+	if(!measure){
+    alert("Please input a value !");
+  } else if(measure<001){
+    alert("Values entered must be greater than zero !");
+ }
+  else{
+  
   var ymeters = measure * 0.9144;
   var message = measure + ' Yards converts to ' + ymeters + ' metres.';
 
@@ -39,15 +59,22 @@ function yTom() {
   console.log(message);
   document.getElementById("resultsentence").innerHTML = message;
 }
+}
 //miles to kilometres in a lapse of memory unable to place buttons beside each other
 function mTok() {
 
   var measure = parseInt(document.getElementById("value1").value);
-
+   if(!measure){
+	  alert("Please input a value!");
+  } else if (measure<0.1){
+	  alert("values must be greater than 0");
+  }  
+   else{
   var mkilolmeters = measure * 1.60934;
-  var message = measure + ' Yards converts to ' + mkilolmeters + ' kilometres.';
+  var message = measure + ' Miles converts to ' + mkilolmeters + ' kilometres.';
 
 
   console.log(message);
   document.getElementById("resultsentence").innerHTML = message;
+}
 }
